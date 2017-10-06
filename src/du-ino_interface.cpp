44f20cd8cf7d1b01d14f6671b5585898c7d954fe
Aaron Mavrinac <mavrinac@gmail.com>
2017-10-06 16:01:14 -0400

    Initial import of interface module.

---
+++ src/du-ino_interface.cpp
@@ -0,0 +1,40 @@
+/*
+ * ####                                                ####
+ * ####                                                ####
+ * ####                                                ####      ##
+ * ####                                                ####    ####
+ * ####  ############  ############  ####  ##########  ####  ####
+ * ####  ####    ####  ####    ####  ####  ####        ########
+ * ####  ####    ####  ####    ####  ####  ####        ########
+ * ####  ####    ####  ####    ####  ####  ####        ####  ####
+ * ####  ####    ####  ####    ####  ####  ####        ####    ####
+ * ####  ############  ############  ####  ##########  ####      ####
+ *                             ####                                ####
+ * ################################                                  ####
+ *            __      __              __              __      __       ####
+ *   |  |    |  |    [__)    |_/     (__     |__|    |  |    [__)        ####
+ *   |/\|    |__|    |  \    |  \    .__)    |  |    |__|    |             ##
+ *
+ *
+ * DU-INO Arduino Library - User Interface Module
+ * Aaron Mavrinac <aaron@logick.ca>
+ */
+
+#include "du-ino_ssd1306.h"
+#include "du-ino_encoder.h"
+#include "du-ino_interface.h"
+
+DUINO_Interface::DUINO_Interface()
+  : display(new DUINO_SSD1306())
+  , encoder(new DUINO_Encoder(9, 10, 8))
+{
+
+}
+
+void DUINO_Interface::begin()
+{
+  // initialize display
+  display->begin();
+  display->clear_display(); 
+  display->display();
+}
---
+++ src/du-ino_interface.h
@@ -0,0 +1,42 @@
+/*
+ * ####                                                ####
+ * ####                                                ####
+ * ####                                                ####      ##
+ * ####                                                ####    ####
+ * ####  ############  ############  ####  ##########  ####  ####
+ * ####  ####    ####  ####    ####  ####  ####        ########
+ * ####  ####    ####  ####    ####  ####  ####        ########
+ * ####  ####    ####  ####    ####  ####  ####        ####  ####
+ * ####  ####    ####  ####    ####  ####  ####        ####    ####
+ * ####  ############  ############  ####  ##########  ####      ####
+ *                             ####                                ####
+ * ################################                                  ####
+ *            __      __              __              __      __       ####
+ *   |  |    |  |    [__)    |_/     (__     |__|    |  |    [__)        ####
+ *   |/\|    |__|    |  \    |  \    .__)    |  |    |__|    |             ##
+ *
+ *
+ * DU-INO Arduino Library - User Interface Module
+ * Aaron Mavrinac <aaron@logick.ca>
+ */
+
+#ifndef DUINO_INTERFACE_H_
+#define DUINO_INTERFACE_H_
+
+#include "Arduino.h"
+
+class DUINO_SSD1306;
+class DUINO_Encoder;
+
+class DUINO_Interface {
+ public:
+  DUINO_Interface();
+
+  void begin();
+
+ private:
+  DUINO_SSD1306 * display;
+  DUINO_Encoder * encoder;
+};
+
+#endif // DUINO_INTERFACE_H_
