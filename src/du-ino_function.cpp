1aa01c6ab5f0941ef2e6142aaa725b4b2ed70f41
Aaron Mavrinac <mavrinac@gmail.com>
2017-11-20 13:06:33 -0500

    Digital inputs are active high.

--- src/du-ino_function.cpp
+++ src/du-ino_function.cpp
@@ -69,7 +69,7 @@ void DUINO_Function::begin()
 bool DUINO_Function::gt_read(uint8_t jack)
 {
   if(jack < 4 && switch_config & (1 << jack))
-    return digitalRead(jack) == HIGH ? false : true;
+    return digitalRead(jack) == HIGH ? true : false;
   else
     return false;
 }
