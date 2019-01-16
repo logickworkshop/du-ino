b97c3f953aa44cbf9deb0f230331b47737c866a0
Aaron Mavrinac <mavrinac@gmail.com>
2019-01-15 22:37:50 -0500

    Document jack indicator UI element API.

--- src/du-ino_indicators.h
+++ src/du-ino_indicators.h
@@ -26,14 +26,35 @@
 #include "Arduino.h"
 #include "du-ino_widgets.h"
 
+/** Jack indicator UI element. */
 class DUINO_JackIndicator : public DUINO_DisplayObject
 {
 public:
+  /**
+   * Constructor.
+   *
+   * \param x Position x coordinate.
+   * \param y Position y coordinate.
+   */
   DUINO_JackIndicator(uint8_t x, uint8_t y);
 
+  /**
+   * Set the state of the indicator.
+   *
+   * \param state The state of the indicator (displays a center dot if true).
+   */
   void set(bool state);
+
+  /**
+   * Toggle the state of the indicator.
+   */
   void toggle();
 
+  /**
+   * Get the current state of the indicator.
+   *
+   * \return The current state of the indicator.
+   */
   bool state() const { return state_; }
 
   virtual uint8_t width() const {return 7; }
