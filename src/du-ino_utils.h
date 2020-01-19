b521a28847f7d0df95fc65427edb029eb414b7ab
Aaron Mavrinac <mavrinac@gmail.com>
2020-01-19 18:54:12 -0500

    Add docstrings to utility functions.

--- src/du-ino_utils.h
+++ src/du-ino_utils.h
@@ -25,12 +25,29 @@
 
 #include "Arduino.h"
 
+/**
+ * Clamp a value between given minimum and maximum allowable values.
+ *
+ * \param value The input value.
+ * \param value_min The minimum allowable value.
+ * \param value_max The maximum allowable value.
+ * \return The clamped output value.
+ */
 template <typename T>
 T clamp(T value, T value_min, T value_max)
 {
   return (value < value_min) ? value_min : (value_max < value) ? value_max : value;
 }
 
+/**
+ * Adjust a value within given minimum and maximum allowable values, and return whether the value has changed.
+ *
+ * \param value The value to adjust.
+ * \param delta The amount by which to adjust the value.
+ * \param value_min The minimum allowable value.
+ * \param value_max The maximum allowable value.
+ * \return True if the value has changed.
+ */
 template <typename T>
 bool adjust(T& value, T delta, T value_min, T value_max)
 {
